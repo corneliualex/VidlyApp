23cbb4f9b4b9faa6aa7ed55d8c3ec10cce531b5d
pcorneliu <corneliu@rubrikkgroup.com>
2017-11-01 17:47:09 +0200

    Added new Classes and db connection

--- VidlyApp/Models/Customer.cs
+++ VidlyApp/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,15 @@ namespace VidlyApp.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        //data annotations
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        //By convention : ClassNameId it's recognized by VS and treats it as a foreign key
+        public byte MembershipTypeId { get; set; }//for optimization instead of using the entire Membership object we may only need the foreign key              
     }
 }
