905c7df21d9b2c8fb5e32feb5816ef0de00905ef
pcorneliu <corneliu@rubrikkgroup.com>
2017-11-01 18:08:25 +0200

    aaa

---
+++ VidlyApp/Migrations/201711011557355_UpdateMembershipType.cs
@@ -0,0 +1,20 @@
+namespace VidlyApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET Name='Pay As You Go' where Id=1");
+            Sql("UPDATE MembershipTypes SET Name='Monthly' where Id=2");
+            Sql("UPDATE MembershipTypes SET Name='Quarterly' where Id=3");
+            Sql("UPDATE MembershipTypes SET Name='Annual' where Id=4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
