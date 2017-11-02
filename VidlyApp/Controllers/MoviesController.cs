3de29e618345e7b5866421ad6d28a02a20f07a88
pcorneliu <corneliu@rubrikkgroup.com>
2017-11-02 18:40:08 +0200

    Half of ex is done

--- VidlyApp/Controllers/MoviesController.cs
+++ VidlyApp/Controllers/MoviesController.cs
@@ -12,7 +12,7 @@ namespace VidlyApp.Controllers
 {
     public class MoviesController : Controller
     {
-        private  ApplicationDbContext _context = new ApplicationDbContext();
+        private ApplicationDbContext _context = new ApplicationDbContext();
 
         protected override void Dispose(bool disposing)
         {
@@ -32,73 +32,22 @@ namespace VidlyApp.Controllers
             return View(movie);
         }
 
-        /* private IEnumerable<Movie> GetMovies()
-         {
-             return new List<Movie>()
-             {
-                 new Movie()
-                 {
-                     Id=1,
-                     Name="Shrek"
-                 },
-                 new Movie()
-                 {
-                     Id=2,
-                     Name="Spiderman"
-                 }
-             };
-         }
-         // GET: Movies/Random
-         public ActionResult Random()
-         {
-             var movie = new Movie()
-             {
-                 Name = "Shrek"
-             };
+        public ActionResult New()
+        {
+            var genres = _context.Gendres.ToList();
+            var viewModel = new NewMovieViewModel()
+            {
+                Genres = genres
+            };
+            return View(viewModel);
+        }
 
-             var customers = new List<Customer>() {
-                 new Customer() {Name="Customer 1" },
-                 new Customer() {Name="Customer 2" }
-             };
-
-             var viewModel = new RandomMovieViewModel()
-             {
-                 Movie = movie,
-                 Customers = customers
-             };
-
-             return View(viewModel);
-         }
-          //First code
-          public ActionResult Random()
-         {
-             var movie = new Movie()
-             {
-                 Name = "Shrek"
-             };
-             //Bad approach
-             ViewData["Movie"] = movie; // Movie is magic string 
-             //Bad approach
-             ViewBag.Movie = movie; //Movie is magic property added to the Movie at runtime and we don't get compile time safety
-             //Best approach
-             return View(movie);
-         }
-
-         public ActionResult Edit(int id)
-         {
-             return Content("id=" + id);
-         }
-
-         public ActionResult Index(int? id, string sortBy)
-         {
-             return Content(String.Format("id={0}&sortBy={1}", id, sortBy));
-         }
-
-         //ASP .NET MVC Attribute Route Constraints for more info about regex
-         [Route("movies/released/{year:regex(\\d{4}):range(1800,2020)}/{month:regex(\\d{2}):range(1,12)}")]
-         public ActionResult ByReleaseDate(int? year,int? month)
-         {
-             return Content(String.Format("year={0}&month={1}",year,month));
-         }*/
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
